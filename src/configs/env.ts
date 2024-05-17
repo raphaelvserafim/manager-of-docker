864afcf4f4e0e9cef0852f0f381285081d52fd26
Raphael Serafim <raphaelvserafim@gmail.com>
2024-05-16 21:57:02 -0400

    fix: env port

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -4,6 +4,6 @@ dotenv.config();
 export const envs = {
   token: process.env.ADMIN_KEY as string,
   imageName: process.env.IMAGE_NAME as string,
-  serverPort: process.env.IMAGE_NAME || 3001,
+  serverPort: process.env.PORT || 3001,
 }
 
